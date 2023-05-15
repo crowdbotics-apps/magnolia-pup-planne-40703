a6eaeab5ae730cf1f973f5525738ac462899f369
Crowdbotics <team@crowdbotics.com>
2023-05-15 16:33:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "magnolia-pup-planne-40703",
+  url: "https://magnolia-pup-planne-40703.botics.co",
+  api: "https://magnolia-pup-planne-40703.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
